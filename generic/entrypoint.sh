4e8622689acf2097a5fdc2bd7dfa83646fa6b204
AshiePleb <38188828+AshiePleb@users.noreply.github.com>
2025-08-25 21:57:53 +0100

    Revert "tweaks?"
    This reverts commit eb9e89266690f4c1ec7e2ef5f2969639c4015fb3.

--- generic/entrypoint.sh
+++ generic/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-set -e
 
 cd /home/container
 
-# Make internal Docker IP address available to processes
-export INTERNAL_IP=$(ip route get 1 2>/dev/null | awk '{print $(NF-2); exit}' || echo "127.0.0.1")
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
-# Replace Startup variables with proper escaping
-MODIFIED_STARTUP=$(printf '%s\n' "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
-printf "customer@cubex:~# %s\n" "${MODIFIED_STARTUP}"
+# Replace Startup variables.
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo "customer@cubex:~# ${MODIFIED_STARTUP}"
 
-# Run the Server
-exec bash -c "${MODIFIED_STARTUP}"
+# Run the Server.
+eval ${MODIFIED_STARTUP}
